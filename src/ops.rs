f3705ca3ca767b082d8f82d15f4857fdca81de3d
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-12-28 06:20:14 -0500

    Add Vec3 struct

--- src/ops.rs
+++ src/ops.rs
@@ -10,3 +10,17 @@ impl Vec2 {
         Vec2 { x, y }
     }
 }
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+}

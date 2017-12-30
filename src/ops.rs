6ed37e9aa4169a5fe84be4a5b43f6c3ae7459491
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-12-30 06:20:24 -0500

    Export ops fields

--- src/ops.rs
+++ src/ops.rs
@@ -1,8 +1,8 @@
 #[derive(Clone, Copy)]
 #[repr(C)]
 pub struct Vec2 {
-    x: f32,
-    y: f32,
+    pub x: f32,
+    pub y: f32,
 }
 
 impl Vec2 {
@@ -14,9 +14,9 @@ impl Vec2 {
 #[derive(Clone, Copy)]
 #[repr(C)]
 pub struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vec3 {
@@ -28,8 +28,8 @@ impl Vec3 {
 #[derive(Clone, Copy)]
 #[repr(C)]
 pub struct Mat {
-    x0: f32, x1: f32, x2: f32, x3: f32,
-    y0: f32, y1: f32, y2: f32, y3: f32,
-    z0: f32, z1: f32, z2: f32, z3: f32,
-    w0: f32, w1: f32, w2: f32, w3: f32,
+    pub x0: f32, pub x1: f32, pub x2: f32, pub x3: f32,
+    pub y0: f32, pub y1: f32, pub y2: f32, pub y3: f32,
+    pub z0: f32, pub z1: f32, pub z2: f32, pub z3: f32,
+    pub w0: f32, pub w1: f32, pub w2: f32, pub w3: f32,
 }

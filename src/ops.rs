172bc5273414e1c1e4ab790748ec23a8168602f4
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-12-30 14:10:40 -0500

    Add translation matrix

--- src/ops.rs
+++ src/ops.rs
@@ -59,6 +59,15 @@ impl Mat {
             0.0, 0.0, 0.0, 1.0,
         )
     }
+
+    pub fn translation(x: f32, y: f32, z: f32) -> Mat {
+        Mat::new(
+            1.0, 0.0, 0.0, x,
+            0.0, 1.0, 0.0, y,
+            0.0, 0.0, 1.0, z,
+            0.0, 0.0, 0.0, 1.0,
+        )
+    }
 }
 
 impl std::ops::Mul for Mat {

01524144a2173c17a9a699b84c218c77a40b112e
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-12-30 06:08:50 -0500

    Add matrix type

--- src/ops.rs
+++ src/ops.rs
@@ -24,3 +24,12 @@ impl Vec3 {
         Vec3 { x, y, z }
     }
 }
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct Mat {
+    x0: f32, x1: f32, x2: f32, x3: f32,
+    y0: f32, y1: f32, y2: f32, y3: f32,
+    z0: f32, z1: f32, z2: f32, z3: f32,
+    w0: f32, w1: f32, w2: f32, w3: f32,
+}

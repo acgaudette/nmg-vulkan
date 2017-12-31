88fe3a3e574b3b1c9daaaadad3a86564dc249a2b
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-12-31 03:05:52 -0500

    Implement Add and Sub traits for Vec3

--- src/alg.rs
+++ src/alg.rs
@@ -60,6 +60,30 @@ impl Vec3 {
     }
 }
 
+impl std::ops::Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3::new(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+        )
+    }
+}
+
+impl std::ops::Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3::new(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z,
+        )
+    }
+}
+
 #[derive(Clone, Copy, PartialEq)]
 #[repr(C)]
 pub struct Mat {

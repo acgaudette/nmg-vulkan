be7aa83b10c4c2ed73390861038fd5ef4cc779a5
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-01-01 08:16:49 -0500

    Add logic module

---
+++ src/logic.rs
@@ -0,0 +1,46 @@
+use render;
+
+pub fn init() -> Vec<render::ModelData> {
+    /* Demo model data */
+
+    let pyramid = render::ModelData::new(
+        vec![
+            render::Vertex::new( 0.0,  0.5, 0.5, 1., 0., 0.),
+            render::Vertex::new( 0.5, -0.5, 0.0, 1., 0., 0.),
+            render::Vertex::new(-0.5, -0.5, 0.0, 1., 0., 0.),
+
+            render::Vertex::new( 0.0,  0.5, 0.5, 0., 1., 0.),
+            render::Vertex::new( 0.5, -0.5, 1.0, 0., 1., 0.),
+            render::Vertex::new( 0.5, -0.5, 0.0, 0., 1., 0.),
+
+            render::Vertex::new( 0.0,  0.5, 0.5, 0., 0., 1.),
+            render::Vertex::new(-0.5, -0.5, 1.0, 0., 0., 1.),
+            render::Vertex::new( 0.5, -0.5, 1.0, 0., 0., 1.),
+
+            render::Vertex::new( 0.0,  0.5, 0.5, 1., 1., 0.),
+            render::Vertex::new(-0.5, -0.5, 0.0, 1., 1., 0.),
+            render::Vertex::new(-0.5, -0.5, 1.0, 1., 1., 0.),
+
+            render::Vertex::new( 0.5, -0.5, 0.0, 1., 0., 0.),
+            render::Vertex::new(-0.5, -0.5, 0.0, 1., 0., 0.),
+            render::Vertex::new(-0.5, -0.5, 1.0, 0., 0., 1.),
+
+            render::Vertex::new(-0.5, -0.5, 1.0, 0., 0., 1.),
+            render::Vertex::new( 0.5, -0.5, 1.0, 0., 1., 0.),
+            render::Vertex::new( 0.5, -0.5, 0.0, 1., 0., 0.),
+        ], vec![
+            0u32, 1u32, 2u32,
+            0u32, 4u32, 1u32,
+            0u32, 7u32, 4u32,
+            0u32, 2u32, 7u32,
+            1u32, 2u32, 7u32,
+            7u32, 4u32, 1u32,
+        ],
+    );
+
+    vec![pyramid.clone(), pyramid]
+}
+
+pub fn update() {
+    //
+}

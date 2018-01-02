6088da57c7bd1ac310de90d4f20b39a3ada59915
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-01-02 04:30:59 -0500

    Add memory alignment buffer tests

--- src/util.rs
+++ src/util.rs
@@ -71,3 +71,72 @@ pub unsafe fn aligned_buffer(
 
     Vec::<usize>::from_raw_parts(start, shrinked, shrinked)
 }
+
+#[cfg(test)]
+mod tests {
+    use util::*;
+
+    #[test]
+    fn create_aligned_buffers() {
+        let matrices = [
+            alg::Mat::identity(),
+            alg::Mat::translation(-1., 2., 5.),
+            alg::Mat::translation(8., 3., 3.),
+        ];
+
+        compare_aligned_buffer(64, &matrices);
+        compare_aligned_buffer(128, &matrices);
+        compare_aligned_buffer(256, &matrices);
+    }
+
+    fn compare_aligned_buffer(alignment: usize, matrices: &[alg::Mat]) {
+        let mut raw = unsafe {
+            aligned_buffer(alignment, matrices)
+        };
+
+        let aligned = {
+            let mut result = Vec::<alg::Mat>::with_capacity(matrices.len());
+            let ptr_len = std::mem::size_of::<usize>();
+
+            unsafe {
+                let mut ptr = raw.as_mut_ptr();
+                let offset = (alignment / ptr_len) as isize;
+                let mut start = ptr as usize;
+
+                for i in 0..matrices.len() {
+                    let matrix = *(ptr as *const alg::Mat);
+                    result.push(matrix);
+
+                    let diff = {
+                        let end = ptr as usize;
+                        let diff = (end - start) / ptr_len;
+                        start = end;
+
+                        diff
+                    };
+
+                    eprintln!(
+                        "\n\tmatrix[{}] diff = {} ({}B)\n{}",
+                        i,
+                        diff,
+                        diff * ptr_len,
+                        matrix,
+                    );
+
+                    if i > 0 {
+                        assert!(diff as isize == offset);
+                    }
+
+                    ptr = ptr.offset(offset);
+                }
+            }
+
+            result
+        };
+
+        // Compare matrices
+        for i in 0..aligned.len() {
+            assert!(aligned[i] == matrices[i]);
+        }
+    }
+}

daaa143ea451c10eec6ea32db0c5d070d6730168
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-01-07 02:17:42 -0500

    Use quaternions in transform component

--- src/components/transform.rs
+++ src/components/transform.rs
@@ -5,14 +5,14 @@ use components;
 // Data layout assumes that almost all entities will have this component
 pub struct Manager {
     positions: Vec<alg::Vec3>,
-    rotations: Vec<alg::Mat>, // Stand-in type
-    scales:    Vec<alg::Vec3>,
+    orientations: Vec<alg::Quat>,
+    scales: Vec<alg::Vec3>,
 }
 
 impl components::Component for Manager {
     fn register(&mut self, entity: entity::Handle) {
-        debug_assert!(self.positions.len() == self.rotations.len());
-        debug_assert!(self.rotations.len() == self.scales.len());
+        debug_assert!(self.positions.len() == self.orientations.len());
+        debug_assert!(self.orientations.len() == self.scales.len());
 
         let i = entity.get_index() as usize;
 
@@ -20,7 +20,7 @@ impl components::Component for Manager {
         loop {
             if i >= self.positions.len() {
                 self.positions.push(alg::Vec3::zero());
-                self.rotations.push(alg::Mat::identity());
+                self.orientations.push(alg::Quat::identity());
                 self.scales.push(alg::Vec3::one());
 
                 continue;
@@ -41,30 +41,30 @@ impl Manager {
     pub fn new(hint: usize) -> Manager {
         Manager {
             positions: Vec::with_capacity(hint),
-            rotations: Vec::with_capacity(hint),
-            scales:    Vec::with_capacity(hint),
+            orientations: Vec::with_capacity(hint),
+            scales: Vec::with_capacity(hint),
         }
     }
 
     pub fn set(
         &mut self,
-        entity:   entity::Handle,
+        entity: entity::Handle,
         position: alg::Vec3,
-        rotation: alg::Mat,
-        scale:    alg::Vec3,
+        orientation: alg::Quat,
+        scale: alg::Vec3,
     ) {
         let i = entity.get_index() as usize;
 
         debug_assert!(i < self.positions.len());
 
         self.positions[i] = position;
-        self.rotations[i] = rotation;
+        self.orientations[i] = orientation;
         self.scales[i] = scale;
     }
 
     pub fn get(&self, entity: entity::Handle) -> (
         alg::Vec3,
-        alg::Mat,
+        alg::Quat,
         alg::Vec3,
     ) {
         let i = entity.get_index() as usize;
@@ -73,7 +73,7 @@ impl Manager {
 
         (
             self.positions[i],
-            self.rotations[i],
+            self.orientations[i],
             self.scales[i],
         )
     }
@@ -89,10 +89,10 @@ impl Manager {
     }
 
     pub fn set_orientation_i(&mut self, index: usize, value: alg::Quat) {
-        self.rotations[index] = value;
+        self.orientations[index] = value;
     }
 
     pub fn get_orientation_i(&self, index: usize) -> alg::Quat {
-        self.rotations[index]
+        self.orientations[index]
     }
 }

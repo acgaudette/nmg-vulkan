60341f6b9d9a09477eedc84e1336c3399165c630
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-01-07 04:44:00 -0500

    Add angular rigidbody physics

--- src/components/rigidbody.rs
+++ src/components/rigidbody.rs
@@ -3,25 +3,32 @@ use entity;
 use components;
 
 // Data layout assumes many physics objects (but may still be sparse)
+#[repr(C)]
 pub struct Manager {
-    velocities: Vec<alg::Vec3>,
-    masses:     Vec<f32>,
-    forces:     Vec<alg::Vec3>,
+    forces: Vec<alg::Vec3>,
+    masses: Vec<f32>,
+    lin_velocities: Vec<alg::Vec3>,
+    torques: Vec<alg::Vec3>,
+    ang_velocities: Vec<alg::Vec3>,
 }
 
 impl components::Component for Manager {
     fn register(&mut self, entity: entity::Handle) {
-        debug_assert!(self.velocities.len() == self.masses.len());
-        debug_assert!(self.masses.len() == self.forces.len());
+        debug_assert!(self.forces.len() == self.masses.len());
+        debug_assert!(self.masses.len() == self.lin_velocities.len());
+        debug_assert!(self.lin_velocities.len() == self.torques.len());
+        debug_assert!(self.torques.len() == self.ang_velocities.len());
 
         let i = entity.get_index() as usize;
 
         // Resize array to fit new entity
         loop {
-            if i >= self.velocities.len() {
-                self.velocities.push(alg::Vec3::zero());
-                self.masses.push(0.);
+            if i >= self.forces.len() {
                 self.forces.push(alg::Vec3::zero());
+                self.masses.push(0.);
+                self.lin_velocities.push(alg::Vec3::zero());
+                self.torques.push(alg::Vec3::zero());
+                self.ang_velocities.push(alg::Vec3::zero());
 
                 continue;
             }
@@ -33,31 +40,34 @@ impl components::Component for Manager {
     // TODO: This currently only returns the length of the underlying data
     // structure, not the count of the registered entities
     fn count(&self) -> usize {
-        self.velocities.len()
+        self.forces.len()
     }
 }
 
 impl Manager {
     pub fn new(hint: usize) -> Manager {
         Manager {
-            velocities: Vec::with_capacity(hint),
-            masses:     Vec::with_capacity(hint),
-            forces:     Vec::with_capacity(hint),
+            forces: Vec::with_capacity(hint),
+            masses: Vec::with_capacity(hint),
+            lin_velocities: Vec::with_capacity(hint),
+            torques: Vec::with_capacity(hint),
+            ang_velocities: Vec::with_capacity(hint),
         }
     }
 
     pub fn set(
         &mut self,
         entity: entity::Handle,
-        force:  alg::Vec3,
         mass:   f32,
+        force:  alg::Vec3,
+        torque: alg::Vec3,
     ) {
         let i = entity.get_index() as usize;
-
         debug_assert!(i < self.masses.len());
 
-        self.masses[i] = mass;
         self.forces[i] = force;
+        self.masses[i] = mass;
+        self.torques[i] = torque;
     }
 
     pub fn simulate(
@@ -65,22 +75,47 @@ impl Manager {
         delta: f64,
         transforms: &mut components::transform::Manager,
     ) {
-        debug_assert!(self.velocities.len() == self.masses.len());
-        debug_assert!(self.masses.len() == self.forces.len());
+        debug_assert!(self.forces.len() == self.masses.len());
+        debug_assert!(self.masses.len() == self.lin_velocities.len());
+        debug_assert!(self.lin_velocities.len() == self.torques.len());
+        debug_assert!(self.torques.len() == self.ang_velocities.len());
 
         // Semi-implicit Euler
-        for i in 0..self.velocities.len() {
+        for i in 0..self.forces.len() {
+            /* Linear motion */
+
+            let lin_momentum = self.forces[i] * delta as f32;
+
             assert!(self.masses[i] > 0.);
 
-            let momentum = self.forces[i] * delta as f32;
-
-            self.velocities[i] = self.velocities[i]
-                + momentum / self.masses[i];
+            self.lin_velocities[i] = self.lin_velocities[i]
+                + lin_momentum / self.masses[i];
 
             let position = transforms.get_position_i(i)
-                + self.velocities[i] * delta as f32;
+                + self.lin_velocities[i] * delta as f32;
 
             transforms.set_position_i(i, position);
+
+            /* Angular motion */
+
+            let ang_momentum = self.torques[i] * delta as f32; // do add here ?
+
+            // TODO: Tensor support
+            let inverse_inertia = 6. / self.masses[i];
+
+            self.ang_velocities[i] = self.ang_velocities[i]
+                + ang_momentum * inverse_inertia;
+
+            // Slow!
+            let ang_quat = alg::Quat::angle_axis_raw(
+                self.ang_velocities[i].norm(),
+                self.ang_velocities[i].mag(),
+            );
+
+            let last = transforms.get_orientation_i(i).norm(); // Renormalize
+            let orientation = ang_quat * 0.5 * last; // Integrate
+
+            transforms.set_orientation_i(i, orientation);
         }
     }
 }

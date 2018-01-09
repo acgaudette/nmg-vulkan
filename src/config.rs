72ffce56d56f24b5363e367a8fff81bc08a393e5
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-01-09 03:09:40 -0500

    Use ini explicitly

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,12 @@
-use ini::Ini;
+use ini;
 
 lazy_static! {
-    pub static ref UNIVERSAL_CONFIG: Ini = {
+    pub static ref ENGINE_CONFIG: ini::Ini = {
         load_config("config.ini")
     };
 }
 
-pub fn load_config(filename: &str) -> Ini {
-    let conf = Ini::load_from_file(filename).unwrap();
+pub fn load_config(filename: &str) -> ini::Ini {
+    let conf = ini::Ini::load_from_file(filename).unwrap();
     conf
 }

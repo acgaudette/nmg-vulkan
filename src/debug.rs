feede8b65d92d9723153e17947cc32cedf2f7cdd
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-01-10 20:20:53 -0500

    Split debug code to its own module

---
+++ src/debug.rs
@@ -0,0 +1,62 @@
+use alg;
+use render;
+
+pub struct Handler {
+    #[cfg(debug_assertions)]
+    pub lines: Vec<render::DebugLine>,
+}
+
+impl Handler {
+    pub fn new() -> Handler {
+        #[cfg(debug_assertions)] {
+            Handler {
+                lines: Vec::new(),
+            }
+        }
+
+        #[cfg(not(debug_assertions))] { Handler { } }
+    }
+
+    #[allow(unused_variables)]
+    pub fn add_line(&mut self, line: alg::Line) {
+        #[cfg(debug_assertions)] {
+            self.lines.push(render::DebugLine::new(line, 1., 0., 0.));
+        }
+    }
+
+    #[allow(unused_variables)]
+    pub fn add_cross(
+        &mut self,
+        center: alg::Vec3,
+        size: f32,
+    ) {
+        #[cfg(debug_assertions)] {
+            let scale = 0.5 * size;
+
+            let first = alg::Line::new(
+                center + alg::Vec3::new( scale, 0.,  scale),
+                center + alg::Vec3::new(-scale, 0., -scale),
+            );
+
+            let second = alg::Line::new(
+                center + alg::Vec3::new( scale, 0., -scale),
+                center + alg::Vec3::new(-scale, 0.,  scale),
+            );
+
+            self.lines.push(
+                render::DebugLine::new(first, 1., 0., 0.),
+            );
+
+            self.lines.push(
+                render::DebugLine::new(second, 1., 0., 0.),
+            );
+        }
+    }
+
+    pub fn clear_lines(&mut self) {
+        #[cfg(debug_assertions)] {
+            self.lines.clear();
+        }
+    }
+}
+

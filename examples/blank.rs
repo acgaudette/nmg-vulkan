e25e813a45ae2c675e11344686a84accbd17bbb7
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-01-14 04:43:51 -0500

    Add blank (empty) example

---
+++ examples/blank.rs
@@ -0,0 +1,57 @@
+extern crate nmg_vulkan as nmg;
+
+use nmg::alg;
+use nmg::entity;
+use nmg::render;
+use nmg::components;
+use nmg::debug;
+
+struct Demo { }
+
+impl nmg::Start for Demo {
+    #[allow(unused_variables)]
+    fn start(
+        &mut self,
+        entities:   &mut entity::Manager,
+        components: &mut components::Container,
+    ) { }
+}
+
+impl nmg::Update for Demo {
+    #[allow(unused_variables)]
+    fn update(
+        &mut self,
+        time:  f64,
+        delta: f64,
+        metadata: nmg::Metadata,
+        screen_height: u32,
+        screen_width:  u32,
+        entities:   &mut entity::Manager,
+        components: &mut components::Container,
+        debug: &mut debug::Handler,
+    ) -> render::SharedUBO {
+        render::SharedUBO::new(
+            alg::Mat::identity(),
+            alg::Mat::identity(),
+        )
+    }
+}
+
+impl nmg::FixedUpdate for Demo {
+    #[allow(unused_variables)]
+    fn fixed_update(
+        &mut self,
+        time: f64,
+        fixed_delta: f32,
+        metadata: nmg::Metadata,
+        screen_height: u32,
+        screen_width: u32,
+        entities: &mut entity::Manager,
+        components: &mut components::Container,
+        debug: &mut debug::Handler,
+    ) { }
+}
+
+fn main() {
+    nmg::go(vec![], Demo { })
+}

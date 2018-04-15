e8f4e1749174775747213db5ca4b2b82e65b3de5
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-04-15 00:19:01 -0400

    Add preliminary input example

---
+++ examples/input.rs
@@ -0,0 +1,113 @@
+extern crate nmg_vulkan as nmg;
+
+use nmg::alg;
+use nmg::entity;
+use nmg::render;
+use nmg::components;
+use nmg::components::Component;
+use nmg::input;
+use nmg::debug;
+
+struct Demo {
+    pyramid: Option<entity::Handle>,
+}
+
+impl nmg::Start for Demo {
+    fn start(
+        &mut self,
+        entities:   &mut entity::Manager,
+        components: &mut components::Container,
+    ) {
+        let pyramid = entities.add();
+        components.transforms.register(pyramid);
+        components.draws.register(pyramid, 0);
+        self.pyramid = Some(pyramid);
+    }
+}
+
+impl nmg::Update for Demo {
+    #[allow(unused_variables)]
+    fn update(
+        &mut self,
+        time:  f64,
+        delta: f64,
+        metadata: nmg::Metadata,
+        screen_height: u32,
+        screen_width:  u32,
+        entities:   &mut entity::Manager,
+        components: &mut components::Container,
+        input: &input::Manager,
+        debug: &mut debug::Handler,
+    ) -> render::SharedUBO {
+        let shared_ubo = {
+            let view = alg::Mat::look_at_view(
+                alg::Vec3::fwd() * -2.0, // Camera position
+                alg::Vec3::zero(), // Target position
+                alg::Vec3::up(),
+            );
+
+            let projection = {
+                alg::Mat::perspective(
+                    60.,
+                    screen_width as f32 / screen_height as f32,
+                    0.01,
+                    4.
+                )
+            };
+
+            render::SharedUBO::new(view, projection)
+        };
+
+        components.transforms.set(
+            self.pyramid.unwrap(),
+            alg::Vec3::zero(),
+            alg::Quat::axis_angle(alg::Vec3::up(), (time as f32) * 2.),
+            alg::Vec3::one(),
+        );
+
+        shared_ubo
+    }
+}
+
+impl nmg::FixedUpdate for Demo {
+    #[allow(unused_variables)]
+    fn fixed_update(
+        &mut self,
+        time: f64,
+        fixed_delta: f32,
+        metadata: nmg::Metadata,
+        screen_height: u32,
+        screen_width: u32,
+        entities: &mut entity::Manager,
+        components: &mut components::Container,
+        input: &input::Manager,
+        debug: &mut debug::Handler,
+    ) { }
+}
+
+fn main() {
+    let demo = Demo { pyramid: None };
+    let model_data = get_models();
+    nmg::go(model_data, demo)
+}
+
+fn get_models() -> Vec<render::ModelData> {
+    let pyramid = render::ModelData::new(
+        vec![
+            render::Vertex::new_raw( 0.0,  0.5,  0.0, 1., 1., 0.), // Peak
+            render::Vertex::new_raw( 0.5, -0.5, -0.5, 1., 0., 0.),
+            render::Vertex::new_raw(-0.5, -0.5, -0.5, 1., 0., 1.),
+            render::Vertex::new_raw( 0.5, -0.5,  0.5, 1., 1., 0.),
+            render::Vertex::new_raw(-0.5, -0.5,  0.5, 1., 1., 1.),
+        ], vec![
+            0u32, 1u32, 2u32,
+            0u32, 3u32, 1u32,
+            0u32, 4u32, 3u32,
+            0u32, 2u32, 4u32,
+            1u32, 2u32, 4u32,
+            4u32, 3u32, 1u32,
+        ],
+    );
+
+    vec![pyramid]
+}

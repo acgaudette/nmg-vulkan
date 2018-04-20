05efb69aacc8d8d30976eaf57e07dd2645c80161
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-04-20 01:26:08 -0400

    Update mesh in softbody example

--- examples/softbody.rs
+++ examples/softbody.rs
@@ -179,21 +179,22 @@ fn main() {
 }
 
 fn get_models() -> Vec<render::ModelData> {
-    let pyramid = render::ModelData::new(
+    let pyramid = render::ModelData::new_with_normals(
         vec![
-            render::Vertex::new_raw( 0.0,  0.5,  0.0, 1., 1., 0.), // Peak
-            render::Vertex::new_raw( 0.5, -0.5, -0.5, 1., 0., 0.),
-            render::Vertex::new_raw(-0.5, -0.5, -0.5, 1., 0., 1.),
-            render::Vertex::new_raw( 0.5, -0.5,  0.5, 1., 1., 0.),
-            render::Vertex::new_raw(-0.5, -0.5,  0.5, 1., 1., 1.),
+            render::Vertex::new_raw( 0.0,  0.5,  0.0, 0., 0., 0., 1., 1., 0.),
+            render::Vertex::new_raw(-0.5, -0.5, -0.5, 0., 0., 0., 1., 0., 1.),
+            render::Vertex::new_raw( 0.5, -0.5, -0.5, 0., 0., 0., 1., 0., 0.),
+            render::Vertex::new_raw( 0.5, -0.5,  0.5, 0., 0., 0., 1., 1., 0.),
+            render::Vertex::new_raw(-0.5, -0.5,  0.5, 0., 0., 0., 1., 1., 1.),
         ], vec![
             0u32, 1u32, 2u32,
-            0u32, 3u32, 1u32,
-            0u32, 4u32, 3u32,
-            0u32, 2u32, 4u32,
-            1u32, 2u32, 4u32,
-            4u32, 3u32, 1u32,
+            0u32, 2u32, 3u32,
+            0u32, 3u32, 4u32,
+            0u32, 4u32, 1u32,
+            1u32, 4u32, 2u32,
+            2u32, 4u32, 3u32,
         ],
+        render::NormalMode::Smooth,
     );
 
     vec![pyramid]

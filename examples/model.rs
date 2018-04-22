2eedc01609575ab0fb7937a16c0e02dd412503a0
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-04-22 16:02:38 -0400

    Update cube winding order

--- examples/model.rs
+++ examples/model.rs
@@ -114,39 +114,39 @@ fn get_models() -> Vec<render::ModelData> {
         vec![
             // Front Face
             render::Vertex::new_raw(-0.5,  0.5, -0.5, 0., 0., 0., 1., 1., 1.),
-            render::Vertex::new_raw(-0.5, -0.5, -0.5, 0., 0., 0., 1., 1., 1.),
-            render::Vertex::new_raw( 0.5, -0.5, -0.5, 0., 0., 0., 1., 1., 1.),
             render::Vertex::new_raw( 0.5,  0.5, -0.5, 0., 0., 0., 1., 1., 1.),
+            render::Vertex::new_raw( 0.5, -0.5, -0.5, 0., 0., 0., 1., 1., 1.),
+            render::Vertex::new_raw(-0.5, -0.5, -0.5, 0., 0., 0., 1., 1., 1.),
 
             // Back Face
             render::Vertex::new_raw(-0.5,  0.5, 0.5, 0., 0., 0., 1., 1., 1.),
-            render::Vertex::new_raw( 0.5,  0.5, 0.5, 0., 0., 0., 1., 1., 1.),
-            render::Vertex::new_raw( 0.5, -0.5, 0.5, 0., 0., 0., 1., 1., 1.),
             render::Vertex::new_raw(-0.5, -0.5, 0.5, 0., 0., 0., 1., 1., 1.),
+            render::Vertex::new_raw( 0.5, -0.5, 0.5, 0., 0., 0., 1., 1., 1.),
+            render::Vertex::new_raw( 0.5,  0.5, 0.5, 0., 0., 0., 1., 1., 1.),
 
             // Top Face
             render::Vertex::new_raw(-0.5, 0.5, -0.5, 0., 0., 0., 1., 0., 1.),
-            render::Vertex::new_raw( 0.5, 0.5, -0.5, 0., 0., 0., 1., 0., 1.),
-            render::Vertex::new_raw( 0.5, 0.5,  0.5, 0., 0., 0., 1., 0., 1.),
             render::Vertex::new_raw(-0.5, 0.5,  0.5, 0., 0., 0., 1., 0., 1.),
+            render::Vertex::new_raw( 0.5, 0.5,  0.5, 0., 0., 0., 1., 0., 1.),
+            render::Vertex::new_raw( 0.5, 0.5, -0.5, 0., 0., 0., 1., 0., 1.),
 
             // Bottom Face
             render::Vertex::new_raw(-0.5, -0.5, -0.5, 0., 0., 0., 1., 1., 0.),
-            render::Vertex::new_raw(-0.5, -0.5,  0.5, 0., 0., 0., 1., 1., 0.),
-            render::Vertex::new_raw( 0.5, -0.5,  0.5, 0., 0., 0., 1., 1., 0.),
             render::Vertex::new_raw( 0.5, -0.5, -0.5, 0., 0., 0., 1., 1., 0.),
+            render::Vertex::new_raw( 0.5, -0.5,  0.5, 0., 0., 0., 1., 1., 0.),
+            render::Vertex::new_raw(-0.5, -0.5,  0.5, 0., 0., 0., 1., 1., 0.),
 
             // Left Face
             render::Vertex::new_raw(-0.5,  0.5, -0.5, 0., 0., 0., 1., 0., 0.),
-            render::Vertex::new_raw(-0.5,  0.5,  0.5, 0., 0., 0., 1., 0., 0.),
-            render::Vertex::new_raw(-0.5, -0.5,  0.5, 0., 0., 0., 1., 0., 0.),
             render::Vertex::new_raw(-0.5, -0.5, -0.5, 0., 0., 0., 1., 0., 0.),
+            render::Vertex::new_raw(-0.5, -0.5,  0.5, 0., 0., 0., 1., 0., 0.),
+            render::Vertex::new_raw(-0.5,  0.5,  0.5, 0., 0., 0., 1., 0., 0.),
 
             // Right Face
             render::Vertex::new_raw(0.5,  0.5, -0.5, 0., 0., 0., 1., 0., 0.),
-            render::Vertex::new_raw(0.5, -0.5, -0.5, 0., 0., 0., 1., 0., 0.),
-            render::Vertex::new_raw(0.5, -0.5,  0.5, 0., 0., 0., 1., 0., 0.),
             render::Vertex::new_raw(0.5,  0.5,  0.5, 0., 0., 0., 1., 0., 0.),
+            render::Vertex::new_raw(0.5, -0.5,  0.5, 0., 0., 0., 1., 0., 0.),
+            render::Vertex::new_raw(0.5, -0.5, -0.5, 0., 0., 0., 1., 0., 0.),
         ], vec![
              0,  1,  2,
              2,  3,  0,

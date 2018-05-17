f97ab55995ba2baa5933a9b864a1d3755f835d06
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-05-17 04:24:02 -0400

    Replace bool tuple with new KeyState struct

--- src/input.rs
+++ src/input.rs
@@ -3,9 +3,24 @@ extern crate voodoo_winit as vdw;
 use alg;
 
 pub struct Manager {
-    pub key_pressed_map: [(bool, bool); 12],
+    pub key_map: [KeyState; 12],
     pub cursor_coords: alg::Vec2,
     pub mouse_delta: alg::Vec2,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct KeyState {
+    pub was_pressed: bool,
+    pub pressed: bool,
+}
+
+impl Default for KeyState {
+    fn default() -> KeyState {
+        KeyState {
+            was_pressed: false,
+            pressed: false,
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug, Eq)]
@@ -33,25 +48,29 @@ impl PartialEq for Key {
 impl Manager {
     pub fn new() -> Manager {
         Manager {
-            key_pressed_map: [(false, false); 12],
+            key_map: [KeyState::default(); 12],
             cursor_coords: alg::Vec2::zero(),
             mouse_delta: alg::Vec2::zero(),
         }
     }
 
+    /* Key state */
+
     pub fn key_held(&self, key: Key) -> bool {
-        self.key_pressed_map[key as usize].1
+        self.key_map[key as usize].pressed
     }
 
     pub fn key_pressed(&self, key: Key) -> bool {
-        let vals = self.key_pressed_map[key as usize];
-        !vals.0 && vals.1
+        let key_state = self.key_map[key as usize];
+        !key_state.was_pressed && key_state.pressed
     }
 
     pub fn key_released(&self, key: Key) -> bool {
-        let vals = self.key_pressed_map[key as usize];
-        vals.0 && !vals.1
+        let key_state = self.key_map[key as usize];
+        key_state.was_pressed && !key_state.pressed
     }
+
+    /* Mouse */
 
     pub fn cursor_coords(&self) -> alg::Vec2 {
         self.cursor_coords

1942cf236d7a460a9ec9307b974efada86a87d27
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-05-17 23:47:43 -0400

    Add light set method

--- src/components/light.rs
+++ src/components/light.rs
@@ -27,4 +27,9 @@ impl Manager {
             lights: std::collections::HashMap::with_capacity(hint),
         }
     }
+
+    pub fn set(&mut self, entity: entity::Handle, light: render::Light) {
+        debug_assert!(self.lights.contains_key(&entity));
+        *self.lights.get_mut(&entity).unwrap() = light;
+    }
 }

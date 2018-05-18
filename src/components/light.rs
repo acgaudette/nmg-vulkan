4f8a9598407be33dc51eb548f9344d9a4c8fc8ab
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-05-17 23:59:58 -0400

    Add light instance UBO cull method

--- src/components/light.rs
+++ src/components/light.rs
@@ -32,4 +32,43 @@ impl Manager {
         debug_assert!(self.lights.contains_key(&entity));
         *self.lights.get_mut(&entity).unwrap() = light;
     }
+
+    // Given a position, return the set of lights affecting it
+    pub fn cull(
+        &self,
+        position: alg::Vec3,
+    ) -> [render::Light; render::MAX_INSTANCE_LIGHTS] {
+        let mut instance_lights = [
+            render::Light::none();
+            render::MAX_INSTANCE_LIGHTS
+        ];
+
+        let mut i = 0;
+
+        for (_, light) in &self.lights {
+            // Directional
+            if light.radius == -1.0 {
+                instance_lights[i] = *light; // Set light
+                i = i + 1;
+            }
+
+            // Dummy light
+            else if light.radius == 0.0 {
+                continue;
+            }
+
+            // Point light--check radius for containment
+            else if light.radius > position.dist(light.vector) {
+                instance_lights[i] = *light; // Set light
+                i = i + 1;
+            }
+
+            // Exit after the number of lights per instance is exceeded
+            if i == render::MAX_INSTANCE_LIGHTS {
+                break;
+            }
+        }
+
+        instance_lights
+    }
 }

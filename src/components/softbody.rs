e54cc6e3879e32da95cbe69443b002cef5a6fdb0
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-05-18 22:38:56 -0400

    Split joint constraints to their own iteration loop

--- src/components/softbody.rs
+++ src/components/softbody.rs
@@ -977,7 +977,10 @@ impl Manager {
                     );
                 }
             }
+        }
 
+        // Solve abstracted constraints
+        for _ in 0..ITERATIONS {
             // Solve joint constraints
             self.solve_joints();
         }
